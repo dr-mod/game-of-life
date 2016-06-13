a04bc6edb28de4470d564884661db2f47d5f6339
Dmytro Panin <dmytro.panin@gmail.com>
2016-06-13 18:26:36 +0300

    Refactored

--- gameOfLife.hs
+++ gameOfLife.hs
@@ -1,26 +1,30 @@
 import Control.Concurrent
 
-outerFrame :: (Int, Int) -> [(Int, Int)]
-outerFrame (x, y) = [(f, s) | f <- [x-1..x+1], s <- [y-1..y+1], not (x == f && s == y)]
-
-rmOut :: [[Int]] -> [(Int, Int)] -> [(Int, Int)] 
-rmOut _ [] = []
-rmOut board ((x,y):xys) = if y < 0 || y >= length board 
-								|| x < 0 || x >= length (board!!y)
-								then rmOut board xys 
-								else (x,y) : rmOut board xys
+outerFrame :: (Int, Int) -> (Int, Int) -> [(Int, Int)]
+outerFrame (x, y) (width, height) = [(f, s) | 
+							f <- [x-1..x+1], 
+							s <- [y-1..y+1], 
+							not (x == f && s == y), 
+							not (f < 0 || f >= width),
+							not (s < 0 || s >= height)]
 
 cellStatus :: Int -> Int -> Int
-cellStatus number alive | number == 3 || number == 2 && alive == 1 = 1 | otherwise = 0
+cellStatus number alive | number == 3 || number == 2 && alive == 1 = 1 
+						| otherwise = 0
 
 getValue :: [[Int]] -> (Int, Int) -> Int
 getValue board (x, y) = board !! y !! x
 
-nextStatus :: [[Int]] -> (Int, Int) -> Int
-nextStatus board point = cellStatus (sum (map (getValue board) (rmOut board (outerFrame point)))) (getValue board point)
+nextPointStatus :: [[Int]] -> (Int, Int) -> Int
+nextPointStatus board point = let 
+								getBoardValue = getValue board
+								currentValue = getBoardValue point
+								boardSize = (length (board!!0), length board)
+							  in cellStatus (sum . map getBoardValue $ outerFrame point boardSize) currentValue
 
-goThrough :: [[Int]] -> ((Int, Int) -> Int) -> [[Int]]
-goThrough board nextStatus = [[nextStatus (x, y) | x <- [0..(length (board!!0))-1]] | y <- [0..(length board)-1]]
+goThrough :: [[Int]] -> [[Int]]
+goThrough board = [[nextStatus (x, y) | x <- [0..(length (board!!0))-1]] | y <- [0..(length board)-1]]
+				  where nextStatus = nextPointStatus board
 
 outTrans 0 = ' '
 outTrans 1 = '*'
@@ -33,7 +37,7 @@ run board = do
 	let view = map (\x -> map outTrans x) board
 	putStr "\ESC[H"
 	mapM_ putStrLn view
-	let nextGen = goThrough board (nextStatus board)
+	let nextGen = goThrough board
 	threadDelay 50000 
 	run nextGen
 main = do
